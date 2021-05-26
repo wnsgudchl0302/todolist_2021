5917609790b9b9c76c356fa6cb93060468c66bde
wnsgudchl0302 <wnsgudchl0302@naver.com>
2021-05-26 11:38:23 +0900

    update todolist

--- src/App.js
+++ src/App.js
@@ -3,26 +3,59 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 import { TextField, Typography } from '@material-ui/core';
+import { KeyboardDatePicker, KeyboardTimePicker} from '@material-ui/pickers';
 
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="header">TODO LIST</div>
-      <div className="input_area">
-        <TextField label="제목" size="normal" margin="normal" fullWidth required />
-        <TextField label="상세내용" size="normal" margin="normal" fullWidth multiline/> 
-        <Button  color="primary">
-          Hello World
-        </Button>
+
+  class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        title: "",
+        content: "",
+        startDate: null,
+        startTime: null,
+      }
+    }
+  
+  render(){
+    return (
+      <div className="App">
+        <div className="header">TODO LIST</div>
+        <div className="input_area">
+          <TextField label="제목" size="normal" margin="normal" fullWidth required />
+          <TextField label="상세내용" size="normal" margin="normal" fullWidth multiline/> 
+          <KeyboardDatePicker
+              disableToolbar
+              variant="inline"
+              format="yyyy/MM/DD"
+              margin="normal"
+              label="시작 예정일"
+              onChange={(value)=>console.log(value)}
+              style={{width: '50%'}}
+              KeyboardButtonProps={{
+                'aria-label': 'change date',
+              }}
+            />  
+            <KeyboardTimePicker
+              margin="normal"
+              label="시작시간"
+              variant="inline"
+              onChange={(value)=>console.log(value)}
+              style={{width: '50'}}
+              KeyboardButtonProps={{
+                'aria-label': 'change date',
+              }}
+            />
+        </div>
+        <div className="list_area">리스트 영역</div>
+        <Typography variant="body2" color="textSecondary" align="center">
+          {'Copyright © 최준형 '+new Date().getFullYear()+'.'}
+        </Typography>
       </div>
-      <div className="list_area">리스트 영역</div>
-      <Typography variant="body2" color="textSecondary" align="center">
-        {'Copyright © 최준형 '+new Date().getFullYear()+'.'}
-      </Typography>
-    </div>
-  );
+    );
+  }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
+import "moment/locale/ko";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MuiPickersUtilsProvider utils={MomentUtils}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </MuiPickersUtilsProvider>,
   document.getElementById('root')
 );
 
